e8e279c711d39f2ae78477ee2c4d64d10c835fcb
Lucas Paulo <mesakilive@gmail.com>
2020-12-09 11:02:41 -0300

    Correções gerais de bugs

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,28 +1,29 @@
 export default class Accordion {
     constructor(list) {
-        this.accordionList = document.querySelectorAll(list);
-        this.activeClass = 'ativo';
+      this.accordionList = document.querySelectorAll(list);
+      this.activeClass = 'ativo';
     }
-    
+  
     toggleAccordion(item) {
-        item.classList.toggle(this.activeClass);
-        item.nextElementSibling.classList.toggle(this.activeClass);
+      item.classList.toggle(this.activeClass);
+      item.nextElementSibling.classList.toggle(this.activeClass);
     }
-
-    // Adiciona os eventos ao accordion
+  
+    // adiciona os eventos ao accordion
     addAccordionEvent() {
-        this.accordionList.forEach((item) => {
-            item.addEventListener('click', () => this.toggleAccordion(item));
-        });
+      this.accordionList.forEach((item) => {
+        item.addEventListener('click', () => this.toggleAccordion(item));
+      });
     }
-
-    // Iniciar função
+  
+    // iniciar função
     init() {
-        if (this.accordionList.length) {
-            // Ativar primeiro item
-            this.toggleAccordion(this.accordionList[0]);
-            this.addAccordionEvent();
-        }
-        return this;
+      if (this.accordionList.length) {
+        // ativar primeiro item
+        this.toggleAccordion(this.accordionList[0]);
+        this.addAccordionEvent();
+      }
+      return this;
     }
-}
+  }
+  
